9ae5302bda72e72724884242adf35c1a45cd6027
morpheus6556 <ma.usv@hotmail.com>
2024-02-26 02:44:37 -0300

    UpDate setText

--- src/components/gpHeader/gpHeader.js
+++ src/components/gpHeader/gpHeader.js
@@ -9,9 +9,9 @@ function GpHeader()
 
   return (
     <div class="Group-header">
+      <BtThree setText="Sobre Mim"sizeFont="7"/>
+      <BtThree setText="Habilidades"sizeFont="7"/>
       <BtThree setText="Projetos"sizeFont="7"/>
-      <BtThree setText="Contato"sizeFont="7"/>
-      <BtThree setText="Contato"sizeFont="7"/>
       <BtThree setText="Contato"sizeFont="7"/>
     </div>
   );
