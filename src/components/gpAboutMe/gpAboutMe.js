cbab77d5f549c8edaad471a588ec8f9a879e2c94
morpheus6556 <ma.usv@hotmail.com>
2024-02-27 09:38:31 -0300

    About Me - New Features

--- src/components/gpAboutMe/gpAboutMe.js
+++ src/components/gpAboutMe/gpAboutMe.js
@@ -1,5 +1,5 @@
 import React, { useEffect} from 'react';
-
+import PhotoViewer from '../photoViewer/photoViewer';
 import './gpAboutMe.css';
 import Typewriter from 'typewriter-effect/dist/core';
 
@@ -11,7 +11,7 @@ function GpAboutMe() {
   useEffect(()=>
   {
     new Typewriter('#element', {
-      strings: ['Matheus V. <b>Costa</b>', 'Desenvolvedor','Cientista','Criativo',],
+      strings: ['Matheus V. <b>Costa</b>', 'Desenvolvedor','Ciêntista da Computação','<i>Full Stack Dev</i>',],
       autoStart: true,
       loop: true,
     });
@@ -27,19 +27,30 @@ function GpAboutMe() {
             <figcaption id='element'></figcaption>
           </figure>
           <div className='Group-aboutme'>
-            <img className='Pin-aboutme'  alt="Componente Visual" src="./imgs/pinAboutMe.png"></img>
-            <div className='Group-Title'>   
-              <div className='Title-aboutme'>Sobre Mim</div>         
-              <div className='Subtitle-aboutme'>Matheus Vinicius Costa</div>   
-            </div>
-              <div className='Text-aboutme1'> Olá, me chamo <b>Matheus Costa</b>, sou desenvolvedor de software e um grande entusiasta 
-              da Ciência da Computação, sempre dedicado a melhorar minhas habilidades e desenvolver aplicativos e idéias inteligentes.</div>
-              <div className='Text-aboutme2'> Aqui no meu site, você vai encontrar algumas coisas interessantes <i>sobre mim </i> 
-              então fique a vontade para explorar, talvez eu tenha deixado alguns <b>Easter Eggs</b> por aí...
+            <div className='Group-aboutme_inner'>
+              <img className='Pin-aboutme'  alt="Componente Visual" src="./imgs/pinAboutMe.png"></img>
+              <div className='Group-Title'>   
+                <div className='Title-aboutme'>Sobre Mim</div>         
+                <div className='Subtitle-aboutme'>Matheus Vinicius Costa</div>   
               </div>
+                <div className='Text-aboutme1'> Olá, me chamo <b>Matheus Costa</b>, sou desenvolvedor Full Stack e um grande entusiasta 
+                da Ciência da Computação, sempre dedicado a melhorar minhas habilidades e desenvolver idéias inteligentes.</div>
+                <div className='Text-aboutme2'> Aqui no meu site, você vai encontrar algumas coisas interessantes <i>sobre mim </i> 
+                então fique a vontade para explorar.
+                </div>
+                <PhotoViewer /> 
+            </div>
+            
+            <div className='Group-easycontact'>
+              <img className='Pin-easycontact'  alt="Componente Visual 2" src="./imgs/pinAboutMe.png"></img>
+              <button className="custom-btn btn-3" onClick={() => window.open('https://www.linkedin.com/in/matheus-vin%C3%ADcius-costa-127666189/')}><span>LinkedIn</span></button>
+              <button className="custom-btn btn-3" onClick={() => window.open('https://github.com/matchur')}><span>GitHub</span></button>          </div>
           </div>
+          
         </div>
-      </div>  
+      </div> 
+      
+
     </div>
   );
 }
