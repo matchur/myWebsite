3df335e221655570476945465d1ae5990ae0105c
morpheus6556 <ma.usv@hotmail.com>
2024-03-13 06:32:57 -0300

    Add Custom Domain

---
+++ src/components/imgVisualizer/imgVisualizer.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+import './imgVisualizer.css';
+
+function ImgVisualizer(params) {
+
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default ImgVisualizer;
