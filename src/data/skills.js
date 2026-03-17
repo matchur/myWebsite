312e4be994cdcf8c0e4d542ce99ce3de6c79da8b
Matheus Vinicius da Costa <ma.usv@hotmail.com>
2026-03-16 23:10:41 -0300

    Update: Skills.js

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,18 +1,104 @@
 export const skills = [
   {
     category: 'Sistemas Operacionais',
-    items: ['Windows', 'Ubuntu', 'Debian', 'Kali Linux', 'Android', 'macOS'],
+    items: [
+      'Windows',
+      'Ubuntu',
+      'Debian',
+      'Kali Linux',
+      'macOS',
+      'Android'
+    ],
   },
   {
-    category: 'Linguagens',
-    items: ['JavaScript', 'TypeScript', 'Python', 'Java', 'C#', 'C++'],
+    category: 'Linguagens de Programação',
+    items: [
+      'Uniface',
+      'C',
+      'C++',
+      'C#',
+      'Python',
+      'Java',
+      'JavaScript',
+      'TypeScript',
+      'HTML',
+      'CSS',
+      'Lua',
+      'Arduino'
+    ],
   },
   {
     category: 'Frameworks e Bibliotecas',
-    items: ['React', 'Next.js', 'Spring', 'Node.js'],
+    items: [
+      'React',
+      'Next.js',
+      'Node.js',
+      'Spring',
+      'TensorFlow',
+      'PyTorch'
+    ],
   },
   {
-    category: 'Ferramentas',
-    items: ['Docker', 'PostgreSQL', 'MySQL', 'Git', 'Figma'],
+    category: 'Banco de Dados',
+    items: [
+      'PostgreSQL',
+      'MySQL',
+      'MongoDB',
+      'H2',
+      'PL/SQL'
+    ],
   },
+  {
+    category: 'Dev Tools',
+    items: [
+      'Git',
+      'Docker',
+      'Vite',
+      'DBeaver',
+      'Draw.io'
+    ],
+  },
+  {
+    category: 'Game Development',
+    items: [
+      'Unity',
+      'Godot',
+      'Game Maker'
+    ],
+  },
+  {
+    category: 'Design e Prototipação',
+    items: [
+      'Figma',
+      'Adobe XD',
+      'Adobe Photoshop',
+      'Canva',
+      'Balsamiq'
+    ],
+  },
+  {
+    category: 'Produtividade e Colaboração',
+    items: [
+      'Jira',
+      'Confluence'
+    ],
+  },
+  {
+    category: 'IA e Ferramentas Inteligentes',
+    items: [
+      'ChatGPT',
+      'Claude',
+      'Claude Code',
+      'GitHub Copilot',
+      'Sora'
+    ],
+  },
+  {
+    category: 'Documentação Técnica',
+    items: [
+      'LaTeX',
+      'Sphinx',
+      'MKDocs',
+    ],
+  }, 
 ];
